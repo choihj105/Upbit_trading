7b08ac2268cec17d5ad96d29feb81f494e87d949
choihj105 <li0_0li2@naver.com>
2021-12-02 21:41:06 +0900

    Refactor: Main Class 속 View Class 사용

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
-from tkinter import *
+import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.messagebox as msgbox
+import View
+
 import requests
 import pyupbit
 import pprint
@@ -13,13 +15,16 @@ import threading
 # Main Loop
 
 
-class Main(Tk):
+class Main(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("UPBIT 자동 매매 프로그램")
+        self.title("UPBIT 자동매매 프로그램")
         self.geometry("640x550")
         self.resizable(False, False) # x(너비), y(높이) 값 변경 불가(창 크기 변경 불가)
+        
+        View.View(self)
 
 
 if __name__ == "__main__":
-    Main().mainloop()
+    root = Main()
+    root.mainloop()
